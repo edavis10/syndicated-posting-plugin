18c13c91d22a8e8b4a2aec4647aef2429cda92ce
Eric Davis <edavis@littlestreamsoftware.com>
2007-09-18 18:51:39 +0000

    * Added a function to set the default admin panel options, get the options from   the db * Hooked the WP activate to my init function * Added getAdminOptions() to init()

--- syndicated-posting/php/main.php
+++ syndicated-posting/php/main.php
@@ -8,7 +8,7 @@ if (class_exists("SyndicatedPostingPlugin")) {
 // Hook into the Wordpress Actions and Filters
 if (isset($sp_plugin)) {
   // Actions
-
+  add_action('activate_syndicated-posting/syndicated-posting.php', array(&$sp_plugin,'init'));
   // Filters
 
  }
--- syndicated-posting/php/syndicated-posting.php
+++ syndicated-posting/php/syndicated-posting.php
@@ -1,9 +1,36 @@
 <?php
 if (!class_exists("SyndicatedPostingPlugin")) {
   class SyndicatedPostingPlugin {
+    var $adminOptionsName = "Syndicated_Posting_Admin_Options";
+
     // Constructor
     function SyndicatedPostingPlugin() {
       
+    }
+
+    // Called when the plugin is activated/installed
+    function init(){
+      $this->getAdminOptions();
+    }
+
+    // Returns: Array of admin options
+    function getAdminOptions() {
+      // Default options
+      $syndicatedPostingAdminOptions = array(
+                                             'feed_urls' => '',
+                                             'search_phrases' => '',
+                                             'prospects' => array());
+      // Get options from DB
+      $dbOptions = get_option($this->adminOptionsName);
+      
+      // Set options
+      if (!empty($dbOptions)){
+        foreach($devOptions as $key => $option) {
+          $syndicatedPostingAdminOptions[$key] = $options;
+        }
+      }
+      update_option($this->adminOptionsName,$syndicatedPostingAdminOptions);
+      return $syndicatedPostingAdminOptions;
     }
   } // End class
  }
