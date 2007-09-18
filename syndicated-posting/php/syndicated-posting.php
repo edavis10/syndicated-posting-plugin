58c01d1858f33770b7c1739b5344ea87df01b5c0
Eric Davis <edavis@littlestreamsoftware.com>
2007-09-18 21:20:21 +0000

    * Working on style for admin page

--- syndicated-posting/php/syndicated-posting.php
+++ syndicated-posting/php/syndicated-posting.php
@@ -50,22 +50,72 @@ if (!class_exists("SyndicatedPostingPlugin")) {
 <?php 
    } ?>
 
-<div class=wrap>
+<div class="wrap">
   <form method="post" action="<?php echo $_SERVER["REQUEST_URI"]; ?>">
-    <h2>Syndicated Posting</h2>
-    <h3>Feeds &amp; Search Terms</h3>
-    <p>
-      <textarea name="spFeedUrls" style="width: 80%; height: 100px;"><?php _e(apply_filters('format_to_edit',$spOptions['feed_urls']), 'SyndicatedPostingPlugin') ?></textarea>
-    </p>
-    <p>
-      <textarea name="spSearchPhrases" style="width: 80%; height: 100px;"><?php _e(apply_filters('format_to_edit',$spOptions['search_phrases']), 'SyndicatedPostingPlugin') ?></textarea>
-    </p>
-
-
+    <h2>Feeds &amp; Search Terms</h2>
+    <table class="widefat">
+      <tbody>
+        <tr>
+          <td>
+            Enter <strong>feed URLs</strong>, one per line or comma-separated
+          </td>
+          <td>
+            Enter <strong>search phrases</strong>, one per line or comma-separated
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <textarea name="spFeedUrls" style="width: 100%; height: 100px;"><?php _e(apply_filters('format_to_edit',$spOptions['feed_urls']), 'SyndicatedPostingPlugin') ?></textarea>
+          </td>
+          <td>
+<textarea name="spSearchPhrases" style="width: 100%; height: 100px;"><?php _e(apply_filters('format_to_edit',$spOptions['search_phrases']), 'SyndicatedPostingPlugin') ?></textarea>
+          </td>
+        </tr>
+      </tbody>
+    </table>
     <div class="submit">
       <input type="submit" name="update_syndicatedPostingPluginSettings" value="<?php _e('Update Settings', 'SyndicatedPostingPlugin') ?>" /></div>
   </form>
 </div>
+
+<div class="wrap">
+  <h2>Syndication Prospects</h2>
+  <table class="widefat">
+    <thead>
+      <tr>
+	<th scope="col">Source</th>
+	<th scope="col">Pubdate</th>
+	<th scope="col">Title</th>
+	<th scope="col">Author</th>
+	<th scope="col"/>
+	<th scope="col"/>
+	<th scope="col"/>
+
+      </tr>
+    </thead>
+    <tbody id="the-list">
+      <tr class="alternate" id="post-54">
+	<td style="font-weight:bold">Earth Blog		</td>
+        <td>2007-07-31 4:55:25 am</td>
+	<td>Welcome to Earthzine!		</td>
+	<td>C.J. Man</td>
+	<td><a class="edit" rel="permalink" href="http://www.earthzine.org/2007/07/31/guns-germs-and-steel-by-jared-diamond/">View</a></td>
+	<td><a class="edit" href="post.php?action=edit&post=53">Syndicate</a></td>
+	<td><a onclick="return deleteSomething( 'post', 53, 'You are about to delete this post \'"Guns, Germs and Steel" by Jared Diamond\'.\n\'OK\' to delete, \'Cancel\' to stop.' );" class="delete" href="post.php?action=delete&post=53&_wpnonce=43b533e904">Delete</a></td>
+      </tr>
+      <tr class="" id="post-53">
+	<td style="font-weight:bold">Earth Blog		</td>
+        <td>2007-07-31 4:55:25 am</td>
+	<td>Welcome to Earthzine!		</td>
+	<td>C.J. Man</td>
+	<td><a class="edit" rel="permalink" href="http://www.earthzine.org/2007/07/31/guns-germs-and-steel-by-jared-diamond/">View</a></td>
+	<td><a class="edit" href="post.php?action=edit&post=53">Syndicate</a></td>
+	<td><a onclick="return deleteSomething( 'post', 53, 'You are about to delete this post \'"Guns, Germs and Steel" by Jared Diamond\'.\n\'OK\' to delete, \'Cancel\' to stop.' );" class="delete" href="post.php?action=delete&post=53&_wpnonce=43b533e904">Delete</a></td>
+      </tr>
+    </tbody>
+</table>
+</div>
+
  <?php
 
     }
