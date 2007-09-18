1cc17824e92d6f92728dd5f70168ab1f02882765
Eric Davis <edavis@littlestreamsoftware.com>
2007-09-18 20:27:51 +0000

    * First working draft of an admin page with getting the admin pages settings

--- syndicated-posting/php/main.php
+++ syndicated-posting/php/main.php
@@ -5,10 +5,26 @@ if (class_exists("SyndicatedPostingPlugin")) {
   $sp_plugin = new SyndicatedPostingPlugin();
  }
 
+// Initialize the admin panel
+if (!function_exists("SyndicatedPostingPlugin_admin")) {
+  function SyndicatedPostingPlugin_admin() {
+    global $sp_plugin;
+    if (!isset($sp_plugin)) {
+      return;
+    }
+    // TODO: Change to management page
+    if (function_exists('add_options_page')) {
+      add_options_page('Syndication Posting', 'Syndication', 9, basename(__FILE__), array(&$sp_plugin, 'printAdminPage'));
+    }
+  }
+ }
+
+
 // Hook into the Wordpress Actions and Filters
 if (isset($sp_plugin)) {
   // Actions
   add_action('activate_syndicated-posting/syndicated-posting.php', array(&$sp_plugin,'init'));
+  add_action('admin_menu', 'SyndicatedPostingPlugin_admin');
   // Filters
 
  }
--- syndicated-posting/php/syndicated-posting.php
+++ syndicated-posting/php/syndicated-posting.php
@@ -1,7 +1,8 @@
 <?php
 if (!class_exists("SyndicatedPostingPlugin")) {
   class SyndicatedPostingPlugin {
-    var $adminOptionsName = "Syndicated_Posting_Admin_Options";
+    var $adminOptionsName = "syndicated_posting_admin_options";
+    var $options = array();
 
     // Constructor
     function SyndicatedPostingPlugin() {
@@ -15,22 +16,60 @@ if (!class_exists("SyndicatedPostingPlugin")) {
 
     // Returns: Array of admin options
     function getAdminOptions() {
-      // Default options
-      $syndicatedPostingAdminOptions = array(
-                                             'feed_urls' => '',
-                                             'search_phrases' => '',
-                                             'prospects' => array());
-      // Get options from DB
-      $dbOptions = get_option($this->adminOptionsName);
-      
-      // Set options
-      if (!empty($dbOptions)){
-        foreach($devOptions as $key => $option) {
-          $syndicatedPostingAdminOptions[$key] = $options;
-        }
+      $this->options = get_option($this->adminOptionsName);
+
+      // Set defaults if empty
+      if (empty($this->options)){
+        $this->options = array(
+                               'feed_urls' => '',
+                               'search_phrases' => '');
       }
-      update_option($this->adminOptionsName,$syndicatedPostingAdminOptions);
-      return $syndicatedPostingAdminOptions;
+      update_option($this->adminOptionsName,$this->options);
+    }
+
+    function printAdminPage() {
+      $this->getAdminOptions();
+      $spOptions = $this->options;
+      if (isset($_POST['update_syndicatedPostingPluginSettings'])) {
+        if (isset($_POST['spFeedUrls'])) {
+// TODO: escape values / apply_filters?
+          $spOptions['feed_urls'] = apply_filters('contant_save_pre', $_POST['spFeedUrls']);
+        }   
+        if (isset($_POST['spSearchPhrases'])) {
+// TODO: escape values / apply_filters?
+          $spOptions['search_phrases'] = apply_filters('content_save_pre', $_POST['spSearchPhrases']);
+        }   
+        update_option($this->adminOptionsName, $spOptions);
+
+        ?>
+<div class="updated">
+  <p>
+    <strong>
+      <?php _e("Settings Updated.", "SyndicatedPostingPlugin");?>
+    </strong>
+  </p>
+</div>
+<?php 
+   } ?>
+
+<div class=wrap>
+  <form method="post" action="<?php echo $_SERVER["REQUEST_URI"]; ?>">
+    <h2>Syndicated Posting</h2>
+    <h3>Feeds &amp; Search Terms</h3>
+    <p>
+      <textarea name="spFeedUrls" style="width: 80%; height: 100px;"><?php _e(apply_filters('format_to_edit',$spOptions['feed_urls']), 'SyndicatedPostingPlugin') ?></textarea>
+    </p>
+    <p>
+      <textarea name="spSearchPhrases" style="width: 80%; height: 100px;"><?php _e(apply_filters('format_to_edit',$spOptions['search_phrases']), 'SyndicatedPostingPlugin') ?></textarea>
+    </p>
+
+
+    <div class="submit">
+      <input type="submit" name="update_syndicatedPostingPluginSettings" value="<?php _e('Update Settings', 'SyndicatedPostingPlugin') ?>" /></div>
+  </form>
+</div>
+ <?php
+
     }
   } // End class
  }
