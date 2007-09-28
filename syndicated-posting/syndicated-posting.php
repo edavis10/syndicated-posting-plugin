70312ab5e820fdd651bd88f125a5a6c1a7dc8415
Eric Davis <edavis@littlestreamsoftware.com>
2007-09-28 18:31:44 +0000

    * Changed authors

--- syndicated-posting/syndicated-posting.php
+++ syndicated-posting/syndicated-posting.php
@@ -3,7 +3,7 @@
 Plugin Name: Syndicated Posting
 Plugin URI: http://www.littlestreamsoftware.com/syndicated-posting
 Version: 1.0
-Author: <a href="http://www.littlestreamsoftware.com">Eric Davis</a> and <A href="http://www.shaneandpeter.com">Peter Chester</a>
+Author: <a href="http://www.littlestreamsoftware.com">Little Stream Software</a> and <a href="http://www.shaneandpeter.com">Shane and Peter Inc</a>
 Description: Syndicated Posting gives WordPress the ability to read a set of <a href="http://en.wikipedia.org/wiki/Web_syndication">web feeds</a> and post their content as a blog post.  The feeds are stored locally and can be filtered using a set of search terms.  You can find the panel under "Manage" > "Syndication"
 */
 
