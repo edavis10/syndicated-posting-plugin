e3230d2d9f355a3a3855e8ee481abb3ca09b34cb
Eric Davis <edavis@littlestreamsoftware.com>
2007-10-02 21:02:58 +0000

    * Moved the new post image to show by the post title and highlighted new post rows in green, #186

--- syndicated-posting/php/syndicated-posting.php
+++ syndicated-posting/php/syndicated-posting.php
@@ -689,18 +689,20 @@ if (!class_exists("SyndicatedPostingPlugin")) {
     function printFeedItem($post, $post_meta, $css_class) {
 
 ?>        
-          <tr class="<?php echo $css_class;?>" id="post-<?php echo $post['ID'] ?>">
+        <tr class="<?php echo $css_class;?>" id="post-<?php echo $post['ID'] ?>" <?php if ($this->lastPostId < $post['ID']) { echo "style='background-color:#99ff99;'";}?>>
           <td><input type="checkbox" name="delete[]" value="<?php echo $post['ID'];?>" </td>
           <td style="font-weight:bold">
-     <?php if ($this->lastPostId < $post['ID']) { 
-        echo '<img src="' . get_bloginfo('wpurl') . '/wp-content/plugins/syndicated-posting/images/new-post.png" alt="(New Post)" title="(New Post)" />';
-     }?> 
              <a href='<?php echo $post_meta['syndicated_source_link'] ?>' target="_blank">
                <?php echo $post_meta['syndicated_source_title'] ?>
              </a>
           </td>
           <td><?php echo $post['post_date'] ?></td>
-	  <td><a href='<?php echo $post_meta['syndicated_link'] ?>' target="_blank"><?php echo $post['post_title'] ?></a></td>
+	  <td>
+          <?php if ($this->lastPostId < $post['ID']) { 
+             echo '<img src="' . get_bloginfo('wpurl') . '/wp-content/plugins/syndicated-posting/images/new-post.png" alt="(New Post)" title="(New Post)" />';
+          }?> 
+            <a href='<?php echo $post_meta['syndicated_link'] ?>' target="_blank"><?php echo $post['post_title'] ?></a>
+          </td>
 	  <td><?php echo $post_meta['syndicated_author'] ?></td>
 	  <td><a class="edit" rel="permalink" href='<?php echo $post_meta['syndicated_link']?>' target="_blank">View</a></td>
           <td>
