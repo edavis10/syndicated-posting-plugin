31e1a71657a49fcc30faecba6683ac8ac4d410a5
Eric Davis <edavis@littlestreamsoftware.com>
2008-03-10 21:19:27 +0000

    Removed Add Item

--- syndicated-posting/php/syndicated-posting.php
+++ syndicated-posting/php/syndicated-posting.php
@@ -798,7 +798,7 @@ function validate(form) {
 }
 </script>
 <div id="add-feed-item" class="wrap">
-    <h2>Add Item</h2>
+    <h2>Syndicate Single Prospect</h2>
     <form method="post" action="<?php echo $this->url;?>" style="width:100%;" onsubmit="return validate(this)">
       <fieldset>
           <p>
